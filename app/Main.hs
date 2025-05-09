e024b14ce23f09e218f669ab7bf27f7dc2c1f00c
Mustitz <mustitz@gmail.com>
2025-05-09 21:17:14 +0000

    Add C++ parser with C-style comment support
    - Create new CppParser module in src/CppParser.hs
    - Implement parser for C-style comments (/* */)
    - Update cabal file to add library section
    - Add hie.yaml for IDE support
    - Update Main.hs to use the new parser module

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,49 @@
 module Main where
 
+import CppParser
+import Text.Parsec (sourceLine, sourceColumn)
+
+prettyPrintComment :: Comment -> IO ()
+prettyPrintComment (Comment cType pos content) = do
+  putStrLn $ "Type: " ++ show cType
+  putStrLn $ "Position: Line " ++ show (sourceLine pos) ++
+             ", Column " ++ show (sourceColumn pos)
+  putStrLn $ "Content: " ++ content
+  putStrLn ""
+
+prettyPrintCppFile :: CppFile -> IO ()
+prettyPrintCppFile file = do
+  putStrLn $ "File: " ++ filePath file
+  putStrLn $ "Total comments: " ++ show (length $ comments file)
+  putStrLn ""
+
+  putStrLn "Comments:"
+  printCommentsWithIndex 1 (comments file)
+    where printCommentsWithIndex :: Int -> [Comment] -> IO ()
+          printCommentsWithIndex _ [] = return ()
+          printCommentsWithIndex i (c:cs) = do
+            putStrLn $ "Comment " ++ show i ++ ":"
+            prettyPrintComment c
+            printCommentsWithIndex (i+1) cs
+
+testCode :: String
+testCode = "int main() {\n" ++
+          "    /* First comment */\n" ++
+          "    int x = 5;\n" ++
+          "    /* Second comment */\n" ++
+          "    /* Third\n" ++
+          "       multi-line\n" ++
+          "       comment */\n" ++
+          "    return 0;\n" ++
+          "}\n"
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  putStrLn "Testing with sample C++ code:"
+  putStrLn "----------------------------"
+  putStrLn testCode
+  putStrLn ""
+
+  case parseCppFile "(sample)" testCode of
+    Left err -> putStrLn $ "Error: " ++ show err
+    Right cppFile -> prettyPrintCppFile cppFile
---
+++ src/CppParser.hs
@@ -0,0 +1,55 @@
+module CppParser
+  ( CppFile(..)
+  , Comment(..)
+  , CommentType(..)
+  , parseCppFile
+  ) where
+
+import Data.Maybe (catMaybes)
+import Control.Monad (void)
+import Text.Parsec
+import Text.Parsec.String (Parser)
+
+data CppFile = CppFile {
+  filePath   :: FilePath,        -- Source file path
+  comments   :: [Comment],       -- All comments in the file
+  rawContent :: String           -- Original file content (for reference)
+} deriving (Show)
+
+data Comment = Comment {
+  commentType :: CommentType,  -- Type of comment
+  commentPos  :: SourcePos,    -- Starting position in source
+  commentText :: String        -- Content of the comment
+} deriving (Show)
+
+data CommentType =
+  CStyle     -- /* ... */
+  deriving (Show, Eq)
+
+cStyleComment :: Parser Comment
+cStyleComment = do
+  pos <- getPosition
+  _ <- string "/*"
+  content <- manyTill anyChar $ try $ string "*/"
+  return $ Comment CStyle pos content
+
+commentOrChar :: Parser (Maybe Comment)
+commentOrChar = (Just <$> try cStyleComment)
+                <|> (void anyChar >> return Nothing)
+
+commentsParser :: Parser [Comment]
+commentsParser = do
+  results <- many commentOrChar
+  return $ catMaybes results
+
+cppFileParser :: FilePath -> String -> Parser CppFile
+cppFileParser path content = do
+  cmts <- commentsParser
+  return $ CppFile {
+    filePath = path,
+    comments = cmts,
+    rawContent = content
+  }
+
+parseCppFile :: FilePath -> String -> Either ParseError CppFile
+parseCppFile path content = parse (cppFileParser path content) path content
