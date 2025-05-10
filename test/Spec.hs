1d5901402b84474dae03e44a0000320b7ef85329
Mustitz <mustitz@gmail.com>
2025-05-10 10:13:57 +0000

    Expand test coverage for C-style comment parser
    - Add helper functions for testing (testCStyleCommentOK, testCStyleCommentFails,
      testParseCppFileOK, mkCComment)
    - Add more test cases for cStyleComment:
      - Empty comments
      - Multi-line comments
      - Unicode character handling
      - Failure cases (unclosed, malformed)
    - Add test case for parseCppFile with a single comment
    - Refactor existing tests to use the new helper functions
    - Improve test organization and readability

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,25 +5,64 @@ import Test.Hspec
 import Wedsa
 import WedsaTesting (cStyleComment)
 
-myTest :: String -> Either ParseError CppFile
-myTest = parseCppFile "(test)"
+testCStyleCommentOK :: String -> String -> Expectation
+testCStyleCommentOK input expectedContent = do
+  let result = parse cStyleComment "(test)" input
+  case result of
+    Left err -> error $ "Should parse C-style comment: " ++ show err
+    Right comment -> do
+      commentType comment `shouldBe` CStyle
+      commentText comment `shouldBe` expectedContent
+
+testCStyleCommentFails :: String -> Expectation
+testCStyleCommentFails input = do
+  let result = parse cStyleComment "(test)" input
+  case result of
+    Left _ -> return () -- Expected to fail
+    Right _ -> error $ "Parser should fail on malformed input: " ++ input
+
+testParseCppFileOK :: String -> (CppFile -> Expectation) -> Expectation
+testParseCppFileOK input validator = do
+  let result = parseCppFile "(test)" input
+  case result of
+    Left err -> error $ "Should parse file: " ++ show err
+    Right cppFile -> validator cppFile
+
+mkCComment :: String -> String
+mkCComment body = "/*" ++ body ++ "*/"
 
 main :: IO ()
 main = hspec $ do
 
   describe "cStyleComment" $ do
+    it "parses an empty comment" $ do
+      testCStyleCommentOK "/**/" ""
+
     it "parses a simple C-style comment" $ do
-      let testInput = "/* Simple comment */"
-      let result = parse cStyleComment "(test)" testInput
-      case result of
-        Left err -> error $ "Should parse C-style comment: " ++ show err
-        Right comment -> do
-          commentType comment `shouldBe` CStyle
-          commentText comment `shouldBe` " Simple comment "
+      let body = " Simple comment "
+      testCStyleCommentOK (mkCComment body) body
+
+    it "parses a multi-line C-style comment" $ do
+      let body = " First line\n   Second line\n   Third line "
+      testCStyleCommentOK (mkCComment body) body
+
+    it "parses a comment with Unicode characters" $ do
+      let body = " Über résumé пример 你好 "
+      testCStyleCommentOK (mkCComment body) body
+
+    it "fails on unclosed comment" $ do
+      testCStyleCommentFails "/* unclosed comment"
+
+    it "fails on malformed comment /*/'" $ do
+      testCStyleCommentFails "/*/"
 
   describe "parseCppFile" $ do
     it "parses an empty file" $ do
-      let result = myTest ""
-      case result of
-        Left err -> error $ "Should parse empty file: " ++ show err
-        Right cppFile -> length (comments cppFile) `shouldBe` 0
+      testParseCppFileOK "" $ \cppFile ->
+        length (comments cppFile) `shouldBe` 0
+
+    it "parses a file with a single comment" $ do
+      let commentBody = " Test comment "
+      testParseCppFileOK (mkCComment commentBody) $ \cppFile -> do
+        length (comments cppFile) `shouldBe` 1
+        commentText (head $ comments cppFile) `shouldBe` commentBody
